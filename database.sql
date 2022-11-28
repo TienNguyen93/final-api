74b36160d54d0fee2a8d09d733f0899ec911cfb0
TienNguyen93 <nguyentien9933@gmail.com>
2022-11-28 13:23:31 -0500

    add sql file

--- database.sql
+++ database.sql
@@ -24,7 +24,24 @@ alter table customers add column OrderID int;
 alter table customers add constraint FK_CustomerOrder foreign key (OrderID)
 references orders(OrderID);
 
+-- get all the customers
+select * from customers;
 
+-- get single customer
+select * from customers where CustomerID = id;
+
+-- create a customer
+insert into customers (CustomerName, Street, City, State, Zip, Order) values (Parameter1, 
+Parameter2, Parameter3, Parameter4, Parameter5, Parameter6);
+
+-- update a customer
+update customers set columnn = value where CustomerID = id;
+
+-- delete a customer
+delete from customers where CustomerID = id;
+
+-- get customer's order
+select * from orders where id in (select Order from customers where Order.OrderID = id);
 
 
 
